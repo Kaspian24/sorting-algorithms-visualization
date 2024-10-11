464e965095d2df2d04ba7f937cb3fca27c1c6e0c
Marcin <mw302227@student.polsl.pl>
2024-10-11 13:24:51 +0200

    feat: added dark mode (#5)

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { ThemeProvider } from '@renderer/components/ThemeProvider/ThemeProvider'
+import { ModeToggle } from '@renderer/components/ModeToggle/ModeToggle'
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === 'production'
@@ -13,16 +15,23 @@ const TanStackRouterDevtools =
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <header className="flex h-12 items-center border-b">
+          <div className="flex gap-2 p-2">
+            <Link to="/" className="[&.active]:font-bold">
+              Home
+            </Link>{' '}
+            <Link to="/about" className="[&.active]:font-bold">
+              About
+            </Link>
+          </div>
+          <ModeToggle />
+        </header>
+        <main className="flex grow flex-col">
+          <Outlet />
+        </main>
+        <footer className="flex h-16 border-t"></footer>
+      </ThemeProvider>
       <Suspense>
         <TanStackRouterDevtools />
       </Suspense>
